abed628bb380e05bf65493c3f66b0c40f93a5710
Samet Çiftçi <temasictfic@gmail.com>
2025-05-15 05:55:55 +0300

    feat: Add initial VSCode settings and update build.gradle.kts structure

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    }
+}
+
 allprojects {
     repositories {
         google()
